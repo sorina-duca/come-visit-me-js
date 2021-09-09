93c435b7c1b7206e1ee526a058ab23b086c751b5
sorina-duca <sorina.duca@gmail.com>
2021-09-09 09:48:12 +0200

    refactor with async/await

--- src/index.js
+++ src/index.js
@@ -13,62 +13,62 @@ app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
-    user.save()
-        .then(() => {
-            res.send(user);
-        })
-        .catch((error) => {
-            res.status(400).send(error);
-        });
+
+    try {
+        await user.save();
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 });
 
-app.post('/couches', (req, res) => {
+app.post('/couches', async (req, res) => {
     const couch = new Couch(req.body);
-    couch
-        .save()
-        .then(() => {
-            res.status(201).send(couch);
-        })
-        .catch((error) => {
-            console.log(error);
-            res.status(400).send(error);
-        });
+
+    try {
+        await couch.save();
+        res.status(201).send(couch);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id;
 
-    User.findById(_id)
-        .then((user) => {
-            if (!user) {
-                return res.status(404).send();
-            }
-
-            res.send(user);
-        })
-        .catch((e) => res.status(500).send(e));
+    try {
+        const user = await User.findById(_id);
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
-app.get('/couches/:id', (req, res) => {
+app.get('/couches/:id', async (req, res) => {
     const _id = req.params.id;
 
-    Couch.findById(_id)
-        .then((couch) => {
-            if (!couch) {
-                return res.status(404).send();
-            }
+    try {
+        const couch = await Couch.findById(_id);
+        if (!couch) {
+            return res.status(404).send();
+        }
 
-            res.send(couch);
-        })
-        .catch((e) => res.status(500).send(e));
+        res.send(couch);
+    } catch (e) {
+        res.status(500).send(e);
+    }
 });
 
-app.get('/couches', (req, res) => {
-    Couch.find({})
-        .then((couches) => {
-            res.send(couches);
-        })
-        .catch((e) => res.status(500).send(e));
+app.get('/couches', async (req, res) => {
+    try {
+        const couches = await Couch.find({});
+        res.send(couches);
+    } catch (e) {
+        res.status(500).send(e);
+    }
 });
