69f649d12ea9e109cc26d9b913ff2d498297e8fd
sorina-duca <sorina.duca@gmail.com>
2021-09-13 13:22:08 +0200

    add routers

--- src/index.js
+++ src/index.js
@@ -2,145 +2,17 @@ const express = require('express');
 require('./db/odm');
 const User = require('./models/user');
 const Couch = require('./models/couch');
+const userRouter = require('../src/routers/user');
+const couchRouter = require('../src/routers/couch');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(userRouter);
+app.use(couchRouter);
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
-});
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body);
-
-    try {
-        await user.save();
-        res.send(user);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
-
-app.post('/couches', async (req, res) => {
-    const couch = new Couch(req.body);
-
-    try {
-        await couch.save();
-        res.status(201).send(couch);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const user = await User.findById(_id);
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-app.delete('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const user = await User.findByIdAndDelete(_id);
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-app.patch('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const user = await User.findByIdAndUpdate(_id, req.body, {
-            new: true,
-            runValidators: true,
-        });
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-app.patch('/couches/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const couch = await Couch.findByIdAndUpdate(_id, req.body, {
-            new: true,
-            runValidators: true,
-        });
-        if (!couch) {
-            return res.status(404).send();
-        }
-        res.send(couch);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-app.get('/couches/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const couch = await Couch.findById(_id);
-        if (!couch) {
-            return res.status(404).send();
-        }
-
-        res.send(couch);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
-
-app.delete('/couches/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const couch = await Couch.findByIdAndDelete(_id);
-        if (!couch) {
-            return res.status(404).send();
-        }
-
-        res.send(couch);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
-
-app.get('/couches', async (req, res) => {
-    try {
-        const couches = await Couch.find({});
-        res.send(couches);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (e) {
-        res.status(500).send(e);
-    }
 });
---
+++ src/routers/couch.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const Couch = require('../models/couch');
+const router = new express.Router();
+
+router.post('/couches', async (req, res) => {
+    const couch = new Couch(req.body);
+
+    try {
+        await couch.save();
+        res.status(201).send(couch);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.patch('/couches/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const couch = await Couch.findByIdAndUpdate(_id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if (!couch) {
+            return res.status(404).send();
+        }
+        res.send(couch);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.get('/couches/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const couch = await Couch.findById(_id);
+        if (!couch) {
+            return res.status(404).send();
+        }
+
+        res.send(couch);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+router.delete('/couches/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const couch = await Couch.findByIdAndDelete(_id);
+        if (!couch) {
+            return res.status(404).send();
+        }
+
+        res.send(couch);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+router.get('/couches', async (req, res) => {
+    try {
+        const couches = await Couch.find({});
+        res.send(couches);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+module.exports = router;
