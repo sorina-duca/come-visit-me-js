fdfd22eed4113cdf7488fcca7e3fa8cc9af603c3
sorina-duca <sorina.duca@gmail.com>
2021-09-13 16:25:05 +0200

    add password hashing hook on save

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const Team = {
     product: 'product',
@@ -7,7 +8,7 @@ const Team = {
     privacy: 'privacy',
 };
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         unique: true,
@@ -52,5 +53,16 @@ const User = mongoose.model('User', {
         },
     },
 });
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    console.log('working');
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
